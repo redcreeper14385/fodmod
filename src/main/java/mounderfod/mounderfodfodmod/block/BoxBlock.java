bd608a98a627af3c85a80ad403d7de1819d6cc2a
redcreeper14385 <mounderfod@gmail.com>
2021-01-25 16:17:39 +0000

    Crates keep items

--- src/main/java/mounderfod/mounderfodfodmod/block/BoxBlock.java
+++ src/main/java/mounderfod/mounderfodfodmod/block/BoxBlock.java
@@ -1,22 +1,38 @@
 package mounderfod.mounderfodfodmod.block;
 
+import mounderfod.mounderfodfodmod.MounderfodFodmod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventories;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class BoxBlock extends BlockWithEntity {
 
@@ -46,6 +62,33 @@ public class BoxBlock extends BlockWithEntity {
     }
 
     @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof BoxBlockEntity) {
+            BoxBlockEntity boxBlockEntity = (BoxBlockEntity)blockEntity;
+            if (!world.isClient) {
+                ItemStack itemStack = new ItemStack(MounderfodFodmod.BOX_BLOCK_ITEM);
+                CompoundTag compoundTag = boxBlockEntity.serializeInventory(new CompoundTag());
+                if (!compoundTag.isEmpty()) {
+                    itemStack.putSubTag("BlockEntityTag", compoundTag);
+                }
+
+                if (boxBlockEntity.hasCustomName()) {
+                    itemStack.setCustomName(boxBlockEntity.getCustomName());
+                }
+
+                ItemEntity itemEntity = new ItemEntity(world, (double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, itemStack);
+                itemEntity.setToDefaultPickupDelay();
+                world.spawnEntity(itemEntity);
+            } else {
+                boxBlockEntity.checkLootInteraction(player);
+            }
+        }
+
+        super.onBreak(world, pos, state, player);
+    }
+
+    @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient) {
             NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(world, pos);
@@ -64,6 +107,41 @@ public class BoxBlock extends BlockWithEntity {
     @Override
     public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
         return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+    }
+
+    @Environment(EnvType.CLIENT)
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        super.appendTooltip(stack, world, tooltip, options);
+        CompoundTag compoundTag = stack.getSubTag("BlockEntityTag");
+        if (compoundTag != null) {
+            if (compoundTag.contains("LootTable", 8)) {
+                tooltip.add(new LiteralText("???????"));
+            }
+
+            if (compoundTag.contains("Items", 9)) {
+                DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(27, ItemStack.EMPTY);
+                Inventories.fromTag(compoundTag, defaultedList);
+                int i = 0;
+                int j = 0;
+
+                for (ItemStack itemStack : defaultedList) {
+                    if (!itemStack.isEmpty()) {
+                        ++j;
+                        if (i <= 4) {
+                            ++i;
+                            MutableText mutableText = itemStack.getName().shallowCopy();
+                            mutableText.append(" x").append(String.valueOf(itemStack.getCount()));
+                            tooltip.add(mutableText);
+                        }
+                    }
+                }
+
+                if (j - i > 0) {
+                    tooltip.add((new TranslatableText("container.shulkerBox.more", new Object[]{j - i})).formatted(Formatting.ITALIC));
+                }
+            }
+        }
+
     }
 
 }
